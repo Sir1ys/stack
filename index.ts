ea65dc599ccd3b1ea26b47ac99096b9f4f2fd0e4
Sir1ys <sashakoroviy@gmail.com>
2024-03-01 22:41:00 +0000

    code was refactored to switch case statement

--- index.ts
+++ index.ts
@@ -8,17 +8,20 @@ function stack({ stackInstance, action, element }: Props) {
   const insensitiveAction = action.toLowerCase();
   const copyOfTheStack = [...stackInstance];
 
-  if (insensitiveAction === "push") return [...copyOfTheStack, element];
-
-  if (insensitiveAction === "pop") return [...copyOfTheStack].slice(0, -1);
-
-  if (insensitiveAction === "empty check") return copyOfTheStack.length === 0;
-
-  if (insensitiveAction === "size") return copyOfTheStack.length;
-
-  if (insensitiveAction === "peek" && copyOfTheStack.length > 0)
-    return copyOfTheStack[copyOfTheStack.length - 1];
-
-  return [];
+  switch (insensitiveAction) {
+    case "push":
+      return [...copyOfTheStack, element];
+    case "pop":
+      return [...copyOfTheStack].slice(0, -1);
+    case "empty check":
+      return copyOfTheStack.length === 0;
+    case "size":
+      return copyOfTheStack.length;
+    case "peek":
+      if (copyOfTheStack.length > 0)
+        return copyOfTheStack[copyOfTheStack.length - 1];
+    default:
+      return [];
+  }
 }
 module.exports = stack;
