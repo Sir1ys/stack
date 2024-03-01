9dc535bf44b8184bb1406dfbbf11c711d66c993f
Sir1ys <sashakoroviy@gmail.com>
2024-03-01 22:32:49 +0000

    tests for peek action are written and passed

--- index.ts
+++ index.ts
@@ -6,11 +6,19 @@ export type Props = {
 
 function stack({ stackInstance, action, element }: Props) {
   const insensitiveAction = action.toLowerCase();
+  const copyOfTheStack = [...stackInstance];
 
-  if (insensitiveAction === "push") return [...stackInstance, element];
+  if (insensitiveAction === "push") return [...copyOfTheStack, element];
 
-  if (insensitiveAction === "pop") return [...stackInstance].slice(0, -1);
+  if (insensitiveAction === "pop") return [...copyOfTheStack].slice(0, -1);
 
-  if (insensitiveAction === "empty check") return stackInstance.length === 0;
+  if (insensitiveAction === "empty check") return copyOfTheStack.length === 0;
+
+  if (insensitiveAction === "size") return copyOfTheStack.length;
+
+  if (insensitiveAction === "peek" && copyOfTheStack.length > 0)
+    return copyOfTheStack[copyOfTheStack.length - 1];
+
+  return [];
 }
 module.exports = stack;
--- intex.test.js
+++ intex.test.js
@@ -4,7 +4,6 @@ describe("stack", () => {
   test("return stack instance when the action is push", () => {
     expect(stack({ stackInstance: [1], action: "Push" })).toEqual([1]);
   });
-
   test("return stack instance with the new element added to the beginning of the array when the action is push", () => {
     expect(stack({ stackInstance: [1], action: "push", element: 2 })).toEqual([
       1, 2,
@@ -17,7 +16,6 @@ describe("stack", () => {
   test("return stack instance when the action is pop", () => {
     expect(stack({ stackInstance: [], action: "pop" })).toEqual([]);
   });
-
   test("return stack instance with the last element removed when the action is pop", () => {
     expect(stack({ stackInstance: [1, 2], action: "pop" })).toEqual([1]);
     expect(stack({ stackInstance: [1, 2, 3], action: "pop" })).toEqual([1, 2]);
@@ -29,8 +27,26 @@ describe("stack", () => {
   test("return true when the action is empty check and stack is empty", () => {
     expect(stack({ stackInstance: [], action: "Empty cHeck" })).toBe(true);
   });
-
   test("return false when the action is empty check and stack isn't empty", () => {
     expect(stack({ stackInstance: [1, 2], action: "Empty cHeck" })).toBe(false);
+  });
+
+  test("return 0 when the action is size and stack is empty", () => {
+    expect(stack({ stackInstance: [], action: "size" })).toBe(0);
+  });
+  test("return stack's size when the action is size and stack isn't empty", () => {
+    expect(stack({ stackInstance: [1], action: "size" })).toBe(1);
+    expect(stack({ stackInstance: [1, 2], action: "size" })).toBe(2);
+    expect(stack({ stackInstance: [1, 2, 3], action: "size" })).toBe(3);
+  });
+
+  test("return empty array when the action is peek and stack is empty", () => {
+    expect(stack({ stackInstance: [], action: "peek" })).toEqual([]);
+  });
+  test("return first element when the action is peek and stack has just one element", () => {
+    expect(stack({ stackInstance: [1], action: "peek" })).toEqual(1);
+  });
+  test("return last element when the action is peek and stack has more than one element", () => {
+    expect(stack({ stackInstance: [1, 2, 3], action: "peek" })).toEqual(3);
   });
 });
